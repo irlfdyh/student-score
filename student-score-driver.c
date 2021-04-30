fdef0f36b13ec3fc1ea924cbc5aaae2f36eaf4fa
derl18 <chocochips1808@gmail.com>
2021-04-30 22:30:44 +0700

    Menambahkan Kode dalam data

--- student-score-driver.c
+++ student-score-driver.c
@@ -91,6 +91,9 @@ void operation_processor(int option)
 void add_student_data()
 {
 	float nilai_akhir, nilai_tugas, nilai_UTS, nilai_quiz, nilai_UAS;
+		if(configure_file("a")==NULL){
+		throw_exception();
+	}
 printf("Masukan NIM = ");
 scanf("%s", student.NIM);
 printf("Nama = ");
@@ -111,12 +114,26 @@ student.final_exam_score = nilai_UAS;
 student.final_score = nilai_akhir;
 student.q_letter = calculate_Q_letter(nilai_akhir);
 puts("OK");
+save_student_data(student);
 operation_menu();
 }
 
 void get_all_student_data()
 {
-
+	if(configure_file("rb")==NULL){
+		throw_exception();
+	}
+ while (read_student_data()==1){
+ 	printf("NIM=%s\n",student.NIM);
+    printf("Nama=%s\n",student.name);
+    printf("Nilai Tugas=%.2f\n",student.task_score);
+    printf("NIlai Quiz=%.2f\n",student.quiz_score);
+    printf("Nilai UTS=%.2f\n",student.mid_exam_score);
+    printf("Nilai UAS=%.2f\n",student.final_exam_score);
+    printf("NIlai Akhir=%.2f\n",student.final_score);
+    printf("Huruf Mutu=%c\n",student.q_letter);
+    }
+ close_file();
 }
 
 void get_student_data_by_NIM()
